816978777343bbfb846cb5e9ec2d21414f2f7cd6
Nombrede <ian.sanna@outlook.com>
2022-05-21 12:04:53 -0300

    Se sube esqueleto del codigo mas funcion para pasar imagenes a blanco y negro

---
+++ src/filters.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <math.h>       /* sqrt */
+#include <vector>
+#include "filters.h"
+#include <thread>  
+#include <atomic>  
+
+#define BLACK 0
+
+using namespace std;
+
+// COMPLETAR :)
+
+// Filtro plano como ejemplo
+
+void plain(ppm& img, unsigned char c)
+{
+
+	for(int i = 0; i < img.height; i++)
+		for(int j = 0; j < img.width; j++)		
+			img.setPixel(i, j, pixel(c,c,c));
+
+}
+
+void blackWhite(ppm& img){
+
+	for(int i = 0; i < img.height; i++)
+		for(int j = 0; j < img.width; j++){
+
+			int R = img.getPixel(i, j).r;
+			int G =	img.getPixel(i, j).g;
+			int B = img.getPixel(i, j).b;
+
+			int BW = (R + G + B)/3;
+
+			if (BW > 255){
+				BW = 255;
+			}
+
+			img.setPixel(i, j, pixel(BW, BW, BW));
+		}			
+
+}
+
